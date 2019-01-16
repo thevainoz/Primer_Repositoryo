dd712c8206474df4b886a1410d52ef164403e368
21855124 <21855124@N205-23.alumnos.uem.es>
2019-01-16 09:23:51 +0100

    Primer commit

---
+++ src/ejercicio1/Serie.java
@@ -0,0 +1,16 @@
+package ejercicio1;
+
+public class Serie {
+	
+	/*
+	 * Crea un paquete ejercicio1 y dentro crea una clase java Serie
+	 *  que que imprima 14 términos de la serie 12 - 23 - 34 - 45 - 56, etc.
+	 */	
+	
+
+	public static void main(String[] args) {
+			for (int i = 12; i <= 155; i += 11) {
+				System.out.print(i + " ");
+			}
+		}
+}
